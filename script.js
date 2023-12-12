97ec850ef0d7e74d250426b661df4f72b1edf20a
G.Rohith <76099851+ROHITH250@users.noreply.github.com>
2023-12-12 09:48:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+let a = 10;
+let b = 20;
+let c=a+b;
+console.log(c);
+
+let result=((a+b)*c);
+
+console.log(result);
+
+console.log(++b);
+console.log(--a);
+
+console.log(a*b);
+console.log(a-b);
+console.log(b/a);
+console.log(a**b);
+console.log(a%b);
+
+console.log(a==b);
+console.log(a<b);
+console.log(a>b);
+console.log(a=b);
+console.log(a===b);
+console.log(a!=b);
